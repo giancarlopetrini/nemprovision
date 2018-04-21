dc64ba0da3ddac3c109aa67dfae9e8eb49ca4155
giancarlopetrini <gpetrini5@gmail.com>
2018-04-21 12:47:46 -0400

    create wallet and objects with nem helpers directly

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var levelup = require('levelup');
 var leveldown = require('leveldown');
+var encoding = require('encoding-down');
+var randomstring = require('randomstring');
 var nem = require("nem-sdk").default;
 
 var app = express();
 app.use(bodyParser.json())
-var db = levelup(leveldown('./walletdb'));
+var db = levelup(leveldown('./walletdb'), {valueEncoding: 'json'});
 
 app.listen(3000);
+
+// make test user to see for duplicates?
+db.put("test", "test string account");
+db.put("test", "duplicate -- test string account");
 
 app.post("/v1/wallet/:id", (req, res) => {
     var id = req.params.id;
@@ -27,31 +33,25 @@ app.post("/v1/wallet/:id", (req, res) => {
             Hardware wallets = trezor
             */
 
-            var wallet = nem.model.wallet.createPRNG(id, "password", nem.model.network.data.testnet.id);
-            // Create a common object
-            // var common = nem.model.objects.create("common")("walletPassword/password", "");
-
-            // Get the wallet account to decrypt
+            // random string generator for password
+            var randPass = randomstring.generate();
+            var wallet = nem.model.wallet.createPRNG(id, randPass, nem.model.network.data.testnet.id);
             var walletAccount = wallet.accounts[0];
             var walletAddress = wallet.accounts[0].address;
-            console.log(walletAddress);
+            console.log("Wallet Address: ", walletAddress);
+            console.log("Password: -> ", randPass);
 
-            // Decrypt account private key 
-            // nem.crypto.helpers.passwordToPrivatekey(common, walletAccount, "pass:bip32");
+            // create wallet file, add to user obj and store
 
-            // The common object now has a private key
-            // console.log(common)
+            var userObj = {
+                account: walletAddress,
+                walletFile: "wallet filed saved into leveldb?, maybe......"
+                // walletObj: wallet
+            }
 
-            /* var [privateKey, pubKey] = generateKeyPair();
-            console.log("pub key: ", pubKey);
-            console.log("private key:", privateKey);
+            console.log(userObj);
 
-            generateWallet(id, privateKey);
-
-            var address = nem.model.address.toAddress(pubKey, nem.model.network.data.mainnet.id);
-            console.log("account address: ", address);
-
-            if (!nem.model.address.isValid(address)) {
+            if (!nem.model.address.isValid(walletAddress)) {
                 console.log("Address validation?", isValid);
                 res.status(500);
                 res.send("NEM address failed validation");
@@ -59,9 +59,7 @@ app.post("/v1/wallet/:id", (req, res) => {
                 return
             }
 
-
-
-            db.put(id, address, (error) => {
+            db.put(id, {test: 'object'}, (error) => {
                 if (error) {
                     console.log("Unable to add record..", error)
                     res.status(500);
@@ -72,49 +70,13 @@ app.post("/v1/wallet/:id", (req, res) => {
             })
             res.send({
                 id: id,
-                account: address
-            }) */
+                account: walletAddress
+            })
         } else {
             res.send({
                 id: id,
-                account: value.toString()
+                account: value
             })
         }
     });
 });
-
-
-var generateKeyPair = () => {
-    // make random bytes
-    var rBytes = nem.crypto.nacl.randomBytes(32);
-    // convert random to hex (make backup?)
-    var privateKey = nem.utils.convert.ua2hex(rBytes);
-    //generate keypair itself
-    var keyPair = nem.crypto.keyPair.create(privateKey);
-
-    return [privateKey, keyPair.publicKey.toString()]
-}
-
-var generateWallet = (id, privateKey) => {
-    // change password generation?
-    var wallet = nem.model.wallet.importPrivateKey(id, "samplePass", privateKey,
-        nem.model.network.data.mainnet.id);
-    var wordArray = nem.crypto.js.enc.Utf8.parse(JSON.stringify(wallet));
-    var base64 = nem.crypto.js.enc.Base64.stringify(wordArray);
-
-    fs.writeFile("tmp/wallet" + id + ".wlt", base64, (error) => {
-        if (error) return console.log(error);
-    });
-
-
-    console.log("/tmp/wallet" + id + ".wlt created!");
-
-    // Create a common object
-    var common = nem.model.objects.create("common")("walletPassword/samplePass", "");
-    // Get the wallet account to decrypt
-    var walletAccount = wallet.accounts[0];
-    // Decrypt account private key 
-    nem.crypto.helpers.passwordToPrivatekey(common, walletAccount, "pass:enc");
-    // The common object now has a private key
-    console.log(common);
-}
