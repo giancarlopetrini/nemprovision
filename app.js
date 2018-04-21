3e87d5f413b989ec37b2870a31cc1801328b500f
giancarlopetrini <gpetrini5@gmail.com>
2018-04-21 01:08:52 -0400

    add initial wallet file generation

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+var fs = require('fs');
+var levelup = require('levelup');
+var leveldown = require('leveldown');
 
-// parse application/json
+var app = express();
+
 app.use(bodyParser.json())
 
 var nem = require("nem-sdk").default;
-
-var levelup = require('levelup');
-var leveldown = require('leveldown');
 var db = levelup(leveldown('./walletdb'));
 
 app.listen(3000);
@@ -22,11 +18,14 @@ app.post("/v1/wallet/:id", (req, res) => {
     // check if id already has wallet in db
     db.get(id, (error, value) => {
         if (error == "NotFoundError: Key not found in database [" + id + "]") {
-            console.log("Creating wallet for: " +id);
+            console.log("Creating wallet for: " + id);
             // run createWallet code here.. then take output
             // and write to db
-            var pubKey = generateKeyPair();
+            var [privateKey, pubKey] = generateKeyPair();
             console.log("pub key: ", pubKey);
+            console.log("private key:", privateKey);
+
+            generateWallet(id, privateKey);
 
             var address = nem.model.address.toAddress(pubKey, nem.model.network.data.mainnet.id);
             console.log("account address: ", address);
@@ -59,15 +58,26 @@ app.post("/v1/wallet/:id", (req, res) => {
     });
 });
 
-// nem stuff goes here
 var generateKeyPair = () => {
     // make random bytes
     var rBytes = nem.crypto.nacl.randomBytes(32);
     // convert random to hex (make backup?)
     var privateKey = nem.utils.convert.ua2hex(rBytes);
-    console.log("private key:", privateKey);
     //generate keypair itself
     var keyPair = nem.crypto.keyPair.create(privateKey);
 
-    return keyPair.publicKey.toString();
+    return [privateKey, keyPair.publicKey.toString()]
+}
+
+var generateWallet = (id, privateKey) => {
+    // change password generation?
+    var wallet = nem.model.wallet.importPrivateKey(id, "samplePass", privateKey,
+        nem.model.network.data.mainnet.id);
+    var wordArray = nem.crypto.js.enc.Utf8.parse(JSON.stringify(wallet));
+    var base64 = nem.crypto.js.enc.Base64.stringify(wordArray);
+
+    fs.writeFile("tmp/wallet" + id + ".wlt", base64, (error) => {
+        if (error) return console.log(error);
+    });
+    console.log("/tmp/wallet" + id + ".wlt created!");
 }
